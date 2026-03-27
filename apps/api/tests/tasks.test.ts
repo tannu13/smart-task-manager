5a0080b0b39ef76674f9b4f00f84516401f17ce9
Tanuj Pant <tanujpant13@gmail.com>
2026-03-28 05:15:55 +0530

    test(api): add test for task creation with sanitised title

--- apps/api/tests/tasks.test.ts
+++ apps/api/tests/tasks.test.ts
@@ -30,4 +30,20 @@ describe("POST /tasks", () => {
 
     expect(response.status).toBe(400);
   });
+
+  it("should trim the title and insert it in db", async () => {
+    const newTitle = "  Task Test 2  ";
+    const response = await request(app).post("/tasks").send({
+      title: newTitle,
+    });
+
+    expect(response.status).toBe(201);
+    const newTask = response.body.data.task;
+    expect(newTask).toMatchObject({
+      title: newTitle.trim(),
+      id: expect.any(String),
+      isCompleted: false,
+      createdAt: expect.any(String),
+    });
+  });
 });

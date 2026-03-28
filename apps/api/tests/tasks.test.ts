59d64b4525501ace06e3815abbbb687292bd5288
Tanuj Pant <tanujpant13@gmail.com>
2026-03-29 04:12:47 +0530

    test(api): add delete task endpoint tests

--- apps/api/tests/tasks.test.ts
+++ apps/api/tests/tasks.test.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import app from "../src/server";
 import db from "../src/db/connection";
 import { tasks } from "../src/db/schema";
+import { eq } from "drizzle-orm";
 
 describe("POST /tasks", () => {
   it("should create & return a new task and 201 status", async () => {
@@ -79,5 +80,46 @@ describe("GET /tasks", () => {
     expect(response.status).toBe(200);
     expect(firstTask.title).toBe("Task 2");
     expect(secondTask.title).toBe("Task 1");
+  });
+});
+
+describe("DELETE /tasks/:id", () => {
+  it("should return validation error 400 if id is not uuid format", async () => {
+    const response = await request(app).delete("/tasks/1234");
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toMatchObject({
+      error: {
+        code: "VALIDATION_ERROR",
+        message: "Validation failed",
+      },
+    });
+  });
+  it("should return not found 404 error for non-existent task id", async () => {
+    const nonExistentButValidUUID = "1a7473c7-e5eb-484b-b8c7-531add10acea";
+    const response = await request(app).delete(
+      `/tasks/${nonExistentButValidUUID}`,
+    );
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toMatchObject({
+      error: {
+        code: "NOT_FOUND",
+        message: "Task not found",
+      },
+    });
+  });
+  it("should delete an existing task id", async () => {
+    const [newTask] = await db
+      .insert(tasks)
+      .values([{ title: "New Task 15" }])
+      .returning();
+    const response = await request(app).delete(`/tasks/${newTask.id}`);
+
+    expect(response.statusCode).toBe(204);
+    expect(response.text).toBe("");
+
+    const result = await db.query.tasks.findFirst({
+      where: eq(tasks.id, newTask.id),
+    });
+    expect(result).toBeUndefined();
   });
 });
